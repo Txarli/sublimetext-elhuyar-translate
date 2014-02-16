087e5aa10b1b69be605d0e5f4cbafadee376ffa3
Txarli <aitorcurrutia@gmail.com>
2014-02-16 13:15:26 +0100

    HTML parser firs step
    TODO: Clean the translation string, to remove spaces and CR

--- sublime_elhuyar.py
+++ sublime_elhuyar.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
-import urllib.request
-from xml import etree
+import urllib.request as request
+import contextlib
 
 class SublimeElhuyarCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -11,29 +11,23 @@ class SublimeElhuyarCommand(sublime_plugin.TextCommand):
                 word = self.view.substr(sel)
                 position = sel.end()
 
-        #string_to_write = ' --> ' + self.getElhuyarTranslation(word)
-        self.getElhuyarTranslation(word)
-        #self.view.insert(edit, position, string_to_write)
+        string_to_write = ' --> ' + self.getElhuyarTranslation(word)
+        self.view.insert(edit, position, string_to_write)
 
     def getElhuyarTranslation(self, word):
         # We create the url string to use the Elhuyar api
         url_str = 'http://www.euskara.euskadi.net/r59-15172x/eu/hizt_el/emaitza.asp?sarrera=' + word + '&mota=sarrera&term_hizkuntza=G&aplik_hizkuntza='
 
         # We open and save to a string the url
-        file_handle = urllib.request.urlopen(url_str)
-        file_handle_str = file_handle.read().decode("latin-1")
+        with contextlib.closing(request.urlopen(url_str)) as file_handle:
+            file_handle_str = file_handle.read().decode("latin-1")
         print (type(file_handle_str))
 
         # The translation is inside the tag <dt class="ordaina" lang="es"><strong></strong></dt>
         # We parse the HTML file to find that tag
-        html = 1
-        target = None
-        encoding = 'utf-8'
-        parser = etree.XMLParser(recover=True)
-        parser.entity["nbsp"] = chr(160)
-        parser.feed(file_handle_str)
-        parser.close()
-        tree = etree.fromstring(file_handle_str, parser=parser)
-        dt = tree.find("dt/strong")
-        print(dt.text())
-        return dt.text()
+        data_tag = '<dt class="ordaina" lang="es"><strong>'
+        data_end_tag = '</strong></dt>'
+        data_start_pos = file_handle_str.index(data_tag) + len(data_tag)
+        data_end_pos = file_handle_str.index(data_end_tag)
+        translation = file_handle_str[data_start_pos:data_end_pos]
+        return translation
