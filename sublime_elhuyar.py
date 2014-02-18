cea2356df5d5186fa6a1a427df1ef4d8c34c9257
Txarli <aitorcurrutia@gmail.com>
2014-02-18 13:24:42 +0100

    Http error handling first step
    Now it handles http errors, but shows the html source of the error message from Elhuyar. TODO: Write an error message.

--- sublime_elhuyar.py
+++ sublime_elhuyar.py
@@ -1,16 +1,21 @@
 import sublime, sublime_plugin
 import urllib.request as request
+from urllib.error import HTTPError as http_error
 import contextlib
 
 '''
     Parameter definitions
 '''
-# The name the source language setting has
+# The name for the source language setting
 LANGUAGE_SETTING_NAME = 'Language'
 
 # The letters that should appear in the Elhuyar url for the source language
 BASQUE_URL_NAME = 'E'
 SPANISH_URL_NAME = 'G'
+
+# Tags the translation is between of
+DATA_START_TAG = '<dt class="ordaina" lang="es"><strong>'
+DATA_END_TAG = '</strong></dt>'
 
 '''
     Global functions
@@ -32,25 +37,30 @@ class SublimeElhuyarCommand(sublime_plugin.TextCommand):
                 word = self.view.substr(sel)
                 position = sel.end()
 
-        string_to_write = ' --> ' + self.getElhuyarTranslation(word)
+        string_to_write = ' --> ' + self.get_elhuyar_request(word)
         self.view.insert(edit, position, string_to_write)
 
-    def getElhuyarTranslation(self, word):
+    def get_elhuyar_request(self, word):
         # We create the url string to use the Elhuyar api
         language = self.view.settings().get(LANGUAGE_SETTING_NAME)
         url_str = 'http://www.euskara.euskadi.net/r59-15172x/eu/hizt_el/emaitza.asp?sarrera=' + word + '&mota=sarrera&term_hizkuntza=' + language + '&aplik_hizkuntza='
 
         # We open and save to a string the url
-        with contextlib.closing(request.urlopen(url_str)) as file_handle:
-            file_handle_str = file_handle.read().decode("latin-1")
-        print (type(file_handle_str))
+        try:
+            with contextlib.closing(request.urlopen(url_str)) as file_handle:
+                file_handle_str = file_handle.read().decode("latin-1")
+                return parse_elhuyar_translation(file_handle_str)
+            print (type(file_handle_str))
+        except http_error as e:
+            error = e.read().decode("latin-1")
+            sublime.error_message(error)
+            return None
 
+    def parse_elhuyar_translation(self, source):
         # The translation is inside the tag <dt class="ordaina" lang="es"><strong></strong></dt>
         # We parse the HTML file to find that tag
-        data_start_tag = '<dt class="ordaina" lang="es"><strong>'
-        data_end_tag = '</strong></dt>'
-        data_start_pos = file_handle_str.index(data_start_tag) + len(data_start_tag)
-        data_end_pos = file_handle_str.index(data_end_tag)
+        data_start_pos = file_handle_str.index(DATA_START_TAG) + len(DATA_START_TAG)
+        data_end_pos = file_handle_str.index(DATA_END_TAG)
         translation = file_handle_str[data_start_pos:data_end_pos].strip()
         return translation
 
